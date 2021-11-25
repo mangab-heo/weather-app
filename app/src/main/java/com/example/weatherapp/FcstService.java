097e69280191cb0f8bd9cf3ab33d3cff31b843db
mangab0159 <aksrkq93@naver.com>
2021-11-26 06:00:20 +0900

    change MainActivity to kotlin

--- app/src/main/java/com/example/weatherapp/FcstService.java
+++ app/src/main/java/com/example/weatherapp/FcstService.java
@@ -25,13 +25,3 @@ public interface FcstService {
                                         @Query("ny") int y);
 }
 
-interface ArpltnService {
-    @GET("getMsrstnAcctoRltmMesureDnsty")
-    Observable<ArpltnResult> getDnsty(@Query("serviceKey") String serviceKey,
-                          @Query("returnType") String returnType,
-                          @Query("numOfRows") String numOfRows,
-                          @Query("pageNo") String pageNo,
-                          @Query("dataTerm") String dataTerm,
-                          @Query("ver") String ver,
-                          @Query("stationName") String stationName);
-}
--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,144 +1,109 @@
-package com.example.weatherapp;
+package com.example.weatherapp
 
-import static com.example.weatherapp.RetrofitClient.getArpltnService;
-import static com.example.weatherapp.WeatherData.combineWeatherData;
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
+import android.widget.Toast
+import com.example.weatherapp.WeatherGrid.LatXLngY
+import com.trello.rxlifecycle4.components.support.RxAppCompatActivity
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.schedulers.Schedulers
+import java.util.*
 
-import android.content.pm.PackageManager;
-import android.os.Bundle;
-import android.widget.ImageButton;
-import android.widget.Toast;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
-
-import com.trello.rxlifecycle4.components.support.RxAppCompatActivity;
-
-import org.jetbrains.annotations.NotNull;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
-import io.reactivex.rxjava3.core.Observable;
-import io.reactivex.rxjava3.schedulers.Schedulers;
-
-public class MainActivity extends RxAppCompatActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        setActionBar();
-
-        setImageButtonListener();
-
-        drawMainView();
+class MainActivity : RxAppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setActionBar()
+        setImageButtonListener()
+        drawMainView()
     }
 
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        drawMainView();
+    override fun onRestart() {
+        super.onRestart()
+        drawMainView()
     }
 
-    private void setImageButtonListener() {
-        ImageButton imageButton = findViewById(R.id.gps_button);
-        imageButton.setOnClickListener(v -> {
-            Toast.makeText(getApplicationContext(), "날씨 정보를 불러옵니다.", Toast.LENGTH_LONG).show();
-            drawMainView();
-        });
+    private fun setImageButtonListener() {
+        val imageButton = findViewById<ImageButton>(R.id.gps_button)
+        imageButton.setOnClickListener { v: View? ->
+            Toast.makeText(applicationContext, "날씨 정보를 불러옵니다.", Toast.LENGTH_LONG).show()
+            drawMainView()
+        }
     }
 
-    private void drawMainView() {
-        if (LocationUtil.isLocationPermissionGranted(MainActivity.this)) {
+    private fun drawMainView() {
+        if (LocationUtil.isLocationPermissionGranted(this@MainActivity)) {
             try {
-                WeatherGrid.LatXLngY gridLocation = LocationUtil.getGridLocation(getApplicationContext());
-                String stationName = LocationUtil.getClosestStation(getApplicationContext(), gridLocation);
-
-                callApiAndUpdateView(gridLocation, stationName);
-            } catch (Exception exception) {
-                Toast.makeText(MainActivity.this, exception.getMessage(), Toast.LENGTH_LONG).show();
+                val gridLocation = LocationUtil.getGridLocation(applicationContext)
+                val stationName = LocationUtil.getClosestStation(applicationContext, gridLocation)
+                callApiAndUpdateView(gridLocation, stationName)
+            } catch (exception: Exception) {
+                Toast.makeText(this@MainActivity, exception.message, Toast.LENGTH_LONG).show()
             }
         }
     }
 
-    private void callApiAndUpdateView(WeatherGrid.LatXLngY gridLocation, String stationName) {
-        List<Observable<?>> requests = getObservables(gridLocation, stationName);
-
-        Observable.zip(requests, parsedData -> {
-            List<ViewData> viewDataList = new ArrayList<>();
-            List<WeatherData> weatherDataList = new ArrayList<>();
-
-            for (Object data : parsedData) {
-                if (data == null) continue;
-                if (data instanceof WeatherData)
-                    weatherDataList.add((WeatherData) data);
-                else if (data instanceof ViewData) viewDataList.add((ViewData) data);
+    private fun callApiAndUpdateView(gridLocation: LatXLngY, stationName: String) {
+        val requests = getObservables(gridLocation, stationName)
+        Observable.zip<Any, List<ViewData>>(requests) { parsedData: Array<Any?> ->
+            val viewDataList: MutableList<ViewData> = ArrayList()
+            val weatherDataList: MutableList<WeatherData> = ArrayList()
+            for (data in parsedData) {
+                if (data == null) continue
+                if (data is WeatherData) weatherDataList.add(data) else if (data is ViewData) viewDataList.add(data)
             }
-            viewDataList.add(combineWeatherData(weatherDataList));
-
-            return viewDataList;
-        })
+            viewDataList.add(WeatherData.combineWeatherData(weatherDataList))
+            viewDataList
+        }
                 .subscribeOn(Schedulers.newThread())
                 .observeOn(AndroidSchedulers.mainThread())
                 .compose(bindToLifecycle())
-                .subscribe(viewDataList -> {
-                    for (ViewData viewData : viewDataList)
-                        viewData.updateView(MainActivity.this);
-                }, throwable -> Toast.makeText(MainActivity.this, "날씨 정보를 불러오지 못 했습니다.", Toast.LENGTH_LONG).show());
+                .subscribe({ viewDataList: List<ViewData> -> for (viewData in viewDataList) viewData.updateView(this@MainActivity) }) { throwable: Throwable? -> Toast.makeText(this@MainActivity, "날씨 정보를 불러오지 못 했습니다.", Toast.LENGTH_LONG).show() }
     }
 
-
-    private void setActionBar() {
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.hide();
-        }
+    private fun setActionBar() {
+        val actionBar = supportActionBar
+        actionBar?.hide()
     }
 
-    @NotNull
-    private List<Observable<?>> getObservables(WeatherGrid.LatXLngY gridLocation, String stationName) {
-        int x = (int) gridLocation.x;
-        int y = (int) gridLocation.y;
-
-        String[] baseDateTimeVil = WeatherData.getBaseDateTime(0);
-        String[] baseDateTimeSrt = WeatherData.getBaseDateTime(1);
-
-        List<Observable<?>> requests = new ArrayList<>();
-
-        if (!stationName.equals("")) {
-            Observable<PmData> getDnsty = getArpltnService().getDnsty(
+    private fun getObservables(gridLocation: LatXLngY, stationName: String): List<Observable<*>> {
+        val x = gridLocation.x.toInt()
+        val y = gridLocation.y.toInt()
+        val baseDateTimeVil = WeatherData.getBaseDateTime(0)
+        val baseDateTimeSrt = WeatherData.getBaseDateTime(1)
+        val requests: MutableList<Observable<*>> = ArrayList()
+        if (stationName != "") {
+            val getDnsty = RetrofitClient.getArpltnService().getDnsty(
                     ArpltnClientConstants.SERVICE_KEY, ArpltnClientConstants.RETURN_TYPE,
                     ArpltnClientConstants.NUM_OF_ROWS, ArpltnClientConstants.PAGE_NO,
                     ArpltnClientConstants.DATA_TERM, ArpltnClientConstants.VER,
                     stationName)
-                    .map(ArpltnResult::toAppData);
-            requests.add(getDnsty);
-        } else Toast.makeText(MainActivity.this, "미세먼지 관측소 정보를 찾지 못했습니다.", Toast.LENGTH_LONG).show();
-
-        Observable<WeatherData> getVillageFcst = RetrofitClient.getFcstService().getVilageFcst(
+                    .map { obj: ArpltnResult -> obj.toAppData() }
+            requests.add(getDnsty)
+        } else Toast.makeText(this@MainActivity, "미세먼지 관측소 정보를 찾지 못했습니다.", Toast.LENGTH_LONG).show()
+        val getVillageFcst = RetrofitClient.getFcstService().getVilageFcst(
                 FcstClientConstants.SERVICE_KEY, FcstClientConstants.PAGE_NO, FcstClientConstants.NUM_OF_ROWS, FcstClientConstants.DATA_TYPE,
                 baseDateTimeVil[0], baseDateTimeVil[1], x, y)
-                .map(FcstResult::toAppData);
-        requests.add(getVillageFcst);
-
-        Observable<WeatherData> getUltraSrtFcst = RetrofitClient.getFcstService().getUltraSrtFcst(
+                .map { obj: FcstResult -> obj.toAppData() }
+        requests.add(getVillageFcst)
+        val getUltraSrtFcst = RetrofitClient.getFcstService().getUltraSrtFcst(
                 FcstClientConstants.SERVICE_KEY, FcstClientConstants.PAGE_NO, FcstClientConstants.NUM_OF_ROWS, FcstClientConstants.DATA_TYPE,
                 baseDateTimeSrt[0], baseDateTimeSrt[1], x, y)
-                .map(FcstResult::toAppData);
-        requests.add(getUltraSrtFcst);
-
-        return requests;
+                .map { obj: FcstResult -> obj.toAppData() }
+        requests.add(getUltraSrtFcst)
+        return requests
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull @NotNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == LocationUtil.REQUEST_CODE) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                Toast.makeText(MainActivity.this, "권한을 획득했습니다.", Toast.LENGTH_LONG).show();
+            if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this@MainActivity, "권한을 획득했습니다.", Toast.LENGTH_LONG).show()
             }
-            drawMainView();
+            drawMainView()
         }
     }
 }
